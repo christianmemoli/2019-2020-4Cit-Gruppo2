5c4e9567ffa9effcd942915e1f310aa5e9c25b4f
FalcoStar219 <brendon.ff7@gmail.com>
2020-02-20 21:59:17 +0100

    sus

--- Registrazione/src/FileException.java
+++
@@ -1,9 +0,0 @@
-public class FileException extends Exception{
-	private String Mess = "";
-	public FileException (String M)	{
-		this.Mess=M;
-	}
-	public String getMess(){
-		return Mess;
-	}
-}
--- Storico Ordini/src/FileException.java
+++
@@ -1,9 +0,0 @@
-public class FileException extends Exception{
-	private String mess = "";
-	public FileException (String M)	{
-		this.mess=M;
-	}
-	public String getMess(){
-		return mess;
-	}
-}
